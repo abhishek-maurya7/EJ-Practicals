1f1dc9dd15af7c22bb864fc0d8e69a48ef43be09
Abhishek Maurya <mauryaabhishek279@gmail.com>
2022-09-29 20:23:57 +0530

    Create ValidateServlet.java

---
+++ 2a.RequestDispatcher/ValidateServlet.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ValidateServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        response.setContentType("text/html");
+        try ( PrintWriter out = response.getWriter()) {
+            String username=request.getParameter("un");
+            String password=request.getParameter("pw");
+            if(password.equals("Servlet")) {
+                request.setAttribute("s1username",username);
+                request.setAttribute("s1password",password); 
+                RequestDispatcher rd= request.getRequestDispatcher("/WelcomeServlet");
+                rd.forward(request, response);
+            } else {
+                out.print("Incorrect password");
+                RequestDispatcher rd= request.getRequestDispatcher("/index.html");
+                rd.include(request, response);
+            }
+        }
+    }
+}
