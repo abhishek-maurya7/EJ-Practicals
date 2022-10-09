368515f74e64ba697a073b8e42dc57a22cc7d665
Abhishek Maurya <mauryaabhishek279@gmail.com>
2022-10-09 20:51:21 +0530

    Create ServletClient.java

---
+++ 7a.ServerHitCounts/ServletClient.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(name= "ServletClient", urlPatterns={"/ServletClient"})
+public class ServletClient extends HttpServlet {
+    @EJB CounterServletHitsBean CounterBean;
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletClient</title>");           
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Number of times this servlet is accessed:" + CounterBean.incrementAndGetHitCount()+"</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+}
