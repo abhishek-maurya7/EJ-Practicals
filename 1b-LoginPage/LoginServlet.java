0b4c24a9c5f2f7f6c5f0fa861a4ae42ae66ffa7b
Abhishek Maurya <mauryaabhishek279@gmail.com>
2022-10-13 18:31:44 +0530

    Update LoginServlet.java

--- 1b-LoginPage/LoginServlet.java
+++ 1b-LoginPage/LoginServlet.java
@@ -3,20 +3,19 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class LoginServlet extends HttpServlet {
-      protected void doPost(HttpServletRequest request, HttpServletResponse response)
-              throws ServletException, IOException {
-          processRequest(request, response);
-          response.setContentType("text/html");
-          try ( PrintWriter out = response.getWriter()) {
-              String username = request.getParameter("username");
-              String password = request.getParameter("password");
-              String msg = "";
-              if(username.equals("admin") && password.equals("admin123")) {
-                  msg = "Hello " + username;
-              } else {
-                  msg = "Login Failed";
-              }
-              out.println(msg);
-          }
-      }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            response.setContentType("text/html");
+            try ( PrintWriter out = response.getWriter()) {
+                String username = request.getParameter("username");
+                String password = request.getParameter("password");
+                String msg = "";
+                if(username.equals("admin") && password.equals("admin123")) {
+                    msg = "Hello " + username;
+                } else {
+                    msg = "Login Failed";
+                }
+                out.println(msg);
+            }
+        }
 }
